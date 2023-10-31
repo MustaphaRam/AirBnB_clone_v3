d6fdc223c578d96b9d37ad4cb2580cd59cb03204
MustaphaRam <mustapha.mad.75@gmail.com>
2023-10-31 10:41:49 +0100

    AirBnB_clone_v3

--- models/place.py
+++ models/place.py
@@ -12,14 +12,14 @@ storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
     place_amenity = Table("place_amenity", Base.metadata,
-                        Column("place_id", String(60),
-                                ForeignKey("places.id"),
-                                primary_key=True,
-                                nullable=False),
-                        Column("amenity_id", String(60),
-                                ForeignKey("amenities.id"),
-                                primary_key=True,
-                                nullable=False))
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True,
+                                 nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True,
+                                 nullable=False))
 
 
 class Place(BaseModel, Base):
